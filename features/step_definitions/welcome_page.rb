b308049ddd305c43b9c6a276dcbf1288b2dc5f89
Louise <louiselai88@gmail.com>
2014-07-30 00:29:55 +1000

    second scenario written and failing

--- features/step_definitions/welcome_page.rb
+++ features/step_definitions/welcome_page.rb
@@ -8,3 +8,12 @@ Then(/^I should see a list of countries$/) do
   expect(page).to have_content "Melbourne"
   expect(page).to have_content "Kuala Lumpur"
 end
+
+Given(/^I click on "(.*?)"$/) do |clicky|
+	click_on clicky
+end
+
+Then(/^I should be on the London restaurant page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
--- features/welcome_page.feature
+++ features/welcome_page.feature
@@ -3,3 +3,7 @@ Feature: The first thing you see when visiting zhefood.com
 Scenario: Visiting the welcome page
 	Given I visit the homepage
 	Then I should see a list of countries
+
+Scenario: Clicking on a country
+	Given I click on "London"
+	Then I should be on the London restaurant page
