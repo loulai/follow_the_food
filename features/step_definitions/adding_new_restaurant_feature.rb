6c23519bb17fab00826b422eb72e10e765ceac81
Louise <louiselai88@gmail.com>
2014-08-05 16:23:43 +1000

    scenario for relevant form one passed

--- features/step_definitions/adding_new_restaurant_feature.rb
+++ features/step_definitions/adding_new_restaurant_feature.rb
@@ -1,3 +1,14 @@
 Given(/^I visit the new restaurant page$/) do
   visit '/restaurants/new'
 end
+
+Then(/^I should see a whole bunch of fields to fill in$/) do
+	expect(page).to have_field 'Name'
+	expect(page).to have_field 'Country'
+	expect(page).to have_field 'Description'
+	expect(page).to have_field 'Location'
+end
+
+Then(/^I should see a 'Save Restaurant' button$/) do
+  pending # express the regexp above with the code you wish you had
+end
