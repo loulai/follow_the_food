292d74983b9f235f2222a118fe123517a7d928e1
Louise <louiselai88@gmail.com>
2014-08-05 15:39:07 +1000

    using namespace for the first time for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  # resources :restaurants
-  resources :london 
+namespace :london do
+  resources :restaurants
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
