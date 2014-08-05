ebb082f53c4006d013102fedb0f2eb7dc6a84d1a
Louise <louiselai88@gmail.com>
2014-08-05 16:43:48 +1000

    defined first step

--- features/step_definitions/adding_new_restaurant_feature.rb
+++ features/step_definitions/adding_new_restaurant_feature.rb
@@ -15,10 +15,21 @@ end
 
 Given(/^I visit the homepage after adding a new restaurant$/) do
   visit '/restaurants/new'
-  
+
 end
 
-Then(/^I should see the new restaurant$/) do
+Given(/^I fill in "(.*?)" with "(.*?)"$/) do |field, content|
+  fill_in 'field', with: 'content'
+end
+
+Given(/^I click "(.*?)"$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
+Then(/^I should be redirected to the homepage$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see my newly added restaurant$/) do
+  pending # express the regexp above with the code you wish you had
+end
